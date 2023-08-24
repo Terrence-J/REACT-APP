f2cc1add8ce274910683e7ff98d6188ba81150f4
Terrence-J <109578355+Terrence-J@users.noreply.github.com>
2023-08-24 13:53:45 +0200

    first commit

--- src/Components/edit.js
+++ src/Components/edit.js
@@ -1,78 +1,74 @@
-import React, { useEffect, useState } from 'react'
-import Members from './Members'
-import Uploader from './Uploader'
-
-
-
+import React, { useState } from 'react';
+import Uploader from './Uploader';
+import { useNavigate } from 'react-router-dom';
 
 const Edit = () => {
+  const [nameF, setNameF] = useState(localStorage.getItem('nameF') || '');
+  const [job, setJob] = useState(localStorage.getItem('job') || '');
+  const [image, setImage] = useState();
 
-    const [editId, setEditId] = useState()
-    const [nameF , setNamef] = useState('')
-    const [ job , setJob] = useState('')
-    const [image, setImage] = useState()
-    const [iD, setID]  = useState()
-
- 
-
-
-    
-     const handleName = (event) => {
-        setNamef(event.target.value);
-       };
-
-      const handleJob = (event) => {
-        setJob(event.target.value);
-       };
   
-        const handleFile = (event) => {
-         const file = event.target.files[0];
-        if (file) {
-        const reader = new FileReader();
+   const navigate = useNavigate();
 
-        reader.onload = (e) => {
+  const handleName = (event) => {
+    setNameF(event.target.value);
+  };
+
+  const handleJob = (event) => {
+    setJob(event.target.value);
+  };
+
+  const handleFile = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
         const dataUrl = e.target.result;
         setImage(dataUrl);
       };
-
 
       reader.readAsDataURL(file);
     }
   };
 
-     const handleEdit = (event) => {
+  const handleEdit = (event) => {
+    localStorage.setItem('nameF', nameF);
+    localStorage.setItem('job', job);
+    navigate("/");
 
-        localStorage.setItem(setID,JSON.stringify({
-              picture: image,
-              nameF: nameF,
-              job: job,
-            })
-   
-        
-    );
-   
   };
-
-
 
   return (
     <div>
-        <form>
-            <Uploader onChange={handleFile} />
-            <div className='input-info'>
-            <input className='shadow p-3 mb-5 bg-body-tertiary rounded'
-             id='nameF' type="text" placeholder='Full names' 
-              onChange={handleName}/>
-            <br />
-            <input className='shadow p-3 mb-5 bg-body-tertiary rounded'
-             id='job' type="text" placeholder='Job tiltle' 
-             onChange={handleJob} />
-            <br />
-            <button style={{ marginLeft: 550, marginTop: 30 }}  onClick={handleEdit}>Submit</button>
-            </div>
-        </form>
+      <form>
+        <Uploader onChange={handleFile} />
+        <div className='input-info'>
+          <input
+            className='shadow p-3 mb-5 bg-body-tertiary rounded'
+            id='nameF'
+            type='text'
+            placeholder='Full names'
+            value={nameF}
+            onChange={handleName}
+          />
+          <br />
+          <input
+            className='shadow p-3 mb-5 bg-body-tertiary rounded'
+            id='job'
+            type='text'
+            placeholder='Job title'
+            value={job}
+            onChange={handleJob}
+          />
+          <br />
+          <button style={{ marginLeft: 550, marginTop: 30 }} onClick={handleEdit}>
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Edit
+export default Edit;
